fd298ff007c817331c41f0751a34bb1b872fcc98
Sanket A Darwante <sanketdarwante6@gmail.com>
2021-11-04 15:51:04 -0400

    Removed unused import

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,7 +1,6 @@
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { TestBed } from "@angular/core/testing"
 import { By } from "@angular/platform-browser";
-import { RouterModule } from "@angular/router";
 import { HeroComponent } from "./hero.component"
 
 //Shallow Integration Testing for Hero Component
