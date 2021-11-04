5a6277b8c27109d728838b203f5e23a9d1b3392e
Sanket A Darwante <sanketdarwante6@gmail.com>
2021-11-04 16:08:11 -0400

    Mocking injected service

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,21 +1,39 @@
 import { NO_ERRORS_SCHEMA } from "@angular/core"
 import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { of } from "rxjs";
+import { HeroService } from "../hero.service";
 import { HeroesComponent } from "./heroes.component"
 
 describe('Hereos Component Shallow Integration Test', ()=>{
   let fixture: ComponentFixture<HeroesComponent>;
-
+  let HEROES;
+  let mockHeroService
   beforeEach(()=>{
+
+    HEROES = [
+      {id:1, name:'SpiderDude', strength:8},
+      {id:2, name:'Wonderful Women', strength:24},
+      {id:3, name:'SuperDude', strength:55}
+    ]
+
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
+
     TestBed.configureTestingModule({
       declarations:[HeroesComponent],
-      schemas: [NO_ERRORS_SCHEMA]
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        //long add technique for  service injection.
+        {provide: HeroService , useValue: mockHeroService }
+      ]
     })
 
     fixture = TestBed.createComponent(HeroesComponent);
   })
 
-  it(' ', ()=>{
-
+  it(' should populate heroes after getHeroes is called', ()=>{
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    expect(fixture.componentInstance.heroes.length).toEqual(3);
   })
 
 })
